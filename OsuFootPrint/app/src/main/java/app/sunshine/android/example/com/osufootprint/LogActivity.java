836f281fb2a41e3fb0155f417ae8e3292c430fd6
vincent <visionkan@gmail.com>
2014-10-18 23:01:48 -0400

    basic frame

---
+++ OsuFootPrint/app/src/main/java/app/sunshine/android/example/com/osufootprint/LogActivity.java
@@ -0,0 +1,32 @@
+package app.sunshine.android.example.com.osufootprint;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+/**
+ * Created by Vincent on 14/10/18.
+ */
+public class LogActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.My_wishlist:
+                Intent intent_wishlist = new Intent("com.litreily.WishlistActivity");
+                startActivity(intent_wishlist);
+            case R.id.my_footprints:
+                Intent intent_myfootprints = new Intent("com.litreily.Myfootprints");
+                startActivity(intent_myfootprints);
+            case R.id.pop_footprints:
+                Intent intent_popfootprints = new Intent("com.litreily.PopActivity");
+                startActivity(intent_popfootprints);
+        }
+    }
+}
---
+++ OsuFootPrint/app/src/main/java/app/sunshine/android/example/com/osufootprint/Myfootprints.java
@@ -0,0 +1,30 @@
+package app.sunshine.android.example.com.osufootprint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+/**
+ * Created by Vincent on 14/10/18.
+ */
+public class Myfootprints extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myfootprint);
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+//            case R.id.My_wishlist:
+//                Intent intent_wishlist = new Intent("com.litreily.WishlistActivity");
+//                startActivity(intent_wishlist);
+//            case R.id.my_footprints:
+//                Intent intent_myfootprints = new Intent("com.litreily.Myfootprints");
+//                startActivity(intent_myfootprints);
+            case R.id.back_footprints:
+                Intent intent_logmenu = new Intent("com.litreily.LogActivity");
+                startActivity(intent_logmenu);
+        }
+    }
+}
