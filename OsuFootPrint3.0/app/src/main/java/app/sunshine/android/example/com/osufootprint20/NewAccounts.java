0ebc9efb960a8839a7dc453830242461e01ece44
vincent <visionkan@gmail.com>
2014-11-02 21:38:25 -0500

    database

--- OsuFootPrint3.0/app/src/main/java/app/sunshine/android/example/com/osufootprint20/NewAccounts.java
+++ OsuFootPrint3.0/app/src/main/java/app/sunshine/android/example/com/osufootprint20/NewAccounts.java
@@ -1,5 +1,7 @@
 package app.sunshine.android.example.com.osufootprint20;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,19 +9,60 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import app.sunshine.android.example.com.osufootprint20.R;
 
 public class NewAccounts extends ActionBarActivity {
     private final String TAG = ((Object) this).getClass().getSimpleName();
+    private EditText newUsername;
+    private EditText newPassword;
+    private EditText passwordConfirm;
+    private DatabaseHelper dh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e(TAG, "++NewAccounts created++");
         setContentView(R.layout.activity_new_accounts);
+        newUsername = (EditText) findViewById(R.id.new_username);
+        newPassword = (EditText) findViewById(R.id.new_password);
+        passwordConfirm = (EditText) findViewById(R.id.confirm_password);
+
     }
 
+    private void CreateAccount(){
+        String username = newUsername.getText().toString();
+        String password = newPassword.getText().toString();
+        String confirm = passwordConfirm.getText().toString();
+        if ((password.equals(confirm)) && (!username.equals(""))
+                && (!password.equals("")) && (!confirm.equals(""))) {
+            this.dh = new DatabaseHelper(this);
+            this.dh.insert(username, password);
+            // this.labResult.setText("Added");
+            Toast.makeText(NewAccounts.this, "new record inserted",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        } else if ((username.equals("")) || (password.equals(""))
+                || (confirm.equals(""))) {
+            Toast.makeText(NewAccounts.this, "Missing entry", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (!password.equals(confirm)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("passwords do not match")
+                    .setNeutralButton("Try Again",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                }
+                            })
+
+                    .show();
+        }
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -35,12 +78,11 @@ public class NewAccounts extends ActionBarActivity {
     public void onClick(View view){
         switch (view.getId()){
             case R.id.create_user_button:
-                Intent intent_create_user = new Intent("android.intent.action.LogActivity");
-                startActivity(intent_create_user);
+                CreateAccount();
+                finish();
                 break;
             case R.id.cancel_button:
-                Intent intent_cancel_to_main = new Intent("android.intent.action.MAIN");
-                startActivity(intent_cancel_to_main);
+                finish();
                 break;
         }
     }
