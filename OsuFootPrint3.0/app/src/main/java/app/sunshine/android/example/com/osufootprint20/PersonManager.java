5fd1baebfb08d6a573b04ffda91b58febca0aa9c
vincent <visionkan@gmail.com>
2014-11-14 10:32:16 -0500

    0

--- OsuFootPrint3.0/app/src/main/java/app/sunshine/android/example/com/osufootprint20/PersonManager.java
+++
@@ -1,7 +0,0 @@
-package app.sunshine.android.example.com.osufootprint20;
-
-/**
- * Created by Vincent on 14/11/3.
- */
-public class PersonManager {
-}
--- OsuFootPrint3.0/app/src/main/java/app/sunshine/android/example/com/osufootprint20/Wishlists.java
+++
@@ -1,9 +0,0 @@
-package app.sunshine.android.example.com.osufootprint20;
-
-/**
- * Created by Vincent on 14/11/3.
- */
-public class Wishlists {
-    String place;
-    String done;
-}
